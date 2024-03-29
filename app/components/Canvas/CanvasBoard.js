a179c3e3f72beadda9f7523c330419dbacaaa7c8
Utkarsh <desaiutkarsh542@gmail.com>
2024-03-29 16:55:22 +0530

    UI of Menubar

---
+++ app/components/Canvas/CanvasBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './canvas.module.css'
+
+const CanvasBoard = () => {
+  return (
+    <div className={styles.canvas}>
+        CanvasBoard
+    </div>
+  )
+}
+
+export default CanvasBoard
